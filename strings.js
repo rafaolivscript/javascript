078b7e702ebbc22940dc4b8bb109dfd8507f7988
MacBooK Pro <macbookpro@MacBook-Pro-de-MacBooK.local>
2024-06-15 22:06:08 -0300

    strings Basicas em javascript

---
+++ strings.js
@@ -0,0 +1,18 @@
+const nome = "Rafael"
+console.log(nome)
+
+let outroNome;
+outroNome = "Rafael batista"
+
+
+
+/**
+ * Duas formas de criar variáveis
+ 
+ * a primeira usando CONST voce cria uma variavel inicia ela
+ * e o valor não pode ser alterado
+ 
+ * a segunda forma usando o LET voce cria e não precisa iniciar
+ * e o valor pode ser alterado a qualquer momento
+ 
+ */
